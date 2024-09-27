74dddae16bb263ddd25bd62a7d160cd7bf522840
Neander <b3xlon9@gmail.com>
2024-09-27 15:36:41 +0900

    Added example code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,23 +1,21 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_logcat/flutter_logcat.dart';
+import 'package:loading_progressbar/loading_progressbar.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  Log.configure(visible: kDebugMode, tag: "Neander");
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+      useMaterial3: true,
+    ),
+    home: const MyHomePage(title: 'Flutter LoadingProgressbar Demo'),
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
@@ -30,40 +28,134 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final LoadingProgressbarController controller = LoadingProgressbarController();
+  int count = 5;
+  bool isProgressTextVisible = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    controller
+      ..addEventListener((event, visible, progress) {
+      Log.i("addEventListener.. event:$event, visible:$visible, progress:$progress");
+      },)
+      ..addAnimatedEndListener((visible, progress) {
+        Log.d("addAnimatedEndListener.. visible:$visible, progress:$progress");
+      },);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return LoadingProgressbar(
+      progressbar: (context, progress) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircularProgressIndicator(),
+              Visibility(
+                visible: isProgressTextVisible,
+                child: Text("$progress%",
+                  style: DefaultTextStyle.of(context).style.copyWith(
+                    color: Colors.black,
+                    fontSize: 24.0,
+                    decorationThickness: 0.0
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      controller: controller,
+      transitionDuration: const Duration(seconds: 1),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Home Screen',
+                style: TextStyle(
+                  fontSize: 36.0,
+                ),
+              ),
+              Text(count.toString(),
+                style: const TextStyle(
+                  fontSize: 24.0
+                ),
+              )
+            ],
+          ),
         ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  Log.d("LoadingProgressbar show()");
+                  controller.show();
+                },
+                tooltip: 'Increment',
+                child: const Icon(Icons.visibility),
+              ),
+              const SizedBox(height: 12.0,),
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  Log.d("LoadingProgressbar hide() after 5 seconds");
+                  controller.show();
+                  if (count <= 0) {
+                    count = 6;
+                  }
+
+                  Timer.periodic(const Duration(seconds: 1), (timer) {
+                    if (count <= 0) {
+                      timer.cancel();
+                      controller.hide();
+                    } else {
+                      setState(() {
+                        count--;
+                      });
+                    }
+                  },);
+
+                },
+                tooltip: 'Increment',
+                child: const Icon(Icons.visibility_off),
+              ),
+              const SizedBox(height: 12.0,),
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  controller.show();
+                  isProgressTextVisible = true;
+                  Log.d("LoadingProgressbar increment setProgress(count--)");
+                  Timer.periodic(const Duration(milliseconds: 250), (timer) {
+                    if (controller.getProgress() >= 100) {
+                      timer.cancel();
+                      controller.hide();
+                      isProgressTextVisible = false;
+                    } else {
+                      controller.setProgress(timer.tick);
+                    }
+                  },);
+                },
+                tooltip: 'Increment',
+                child: const Icon(Icons.trending_up_outlined),
+              ),
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
